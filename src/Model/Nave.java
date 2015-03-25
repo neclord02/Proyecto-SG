fd29159b59fd564763639079fbe6c9a151bdb49f
neclord02 <amglez@correo.ugr.es>
2015-03-25 17:03:03 +0100

    Path naves beta

--- src/Model/Nave.java
+++ src/Model/Nave.java
@@ -5,9 +5,14 @@ import com.sun.j3d.loaders.ParsingErrorException;
 import com.sun.j3d.loaders.Scene;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
 import java.io.FileNotFoundException;
+import javax.media.j3d.Alpha;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.PositionPathInterpolator;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 
 public class Nave {
@@ -48,11 +53,30 @@ public class Nave {
     
     // Probablemente deberiamos hacer una clase "Transformaciones"
     public TransformGroup translate(Vector3f vector){
+        
         Transform3D transform3D = new Transform3D();
-        transform3D.setTranslation(vector);
         TransformGroup transformGroup =new TransformGroup();
         transformGroup.setTransform(transform3D);
-        return transformGroup;        
+        transformGroup.setCapability( TransformGroup.ALLOW_TRANSFORM_READ );
+        transformGroup.setCapability( TransformGroup.ALLOW_TRANSFORM_WRITE );
+        
+        Alpha alphaNave = new Alpha( -1, Alpha.INCREASING_ENABLE, 0,0,6000,0,0,0,0,0 );
+        // Nada
+        // Reposo
+        // Lineal
+        // Deceleracion inicial
+        // Cuanto tiempo en MAX
+        // Mismo hasta 0
+        float [] alphas = { 0.0f,0.33f,0.66f,1.0f };
+        Point3f [] positions = { new Point3f(60.0f,60.0f,60.0f),new Point3f(80.0f,80.0f,60.0f),new Point3f(100.0f,60.0f,60.0f),new Point3f(60.0f,60.0f,60.0f) };
+        
+        
+        PositionPathInterpolator path = new PositionPathInterpolator( alphaNave,transformGroup,transform3D,alphas,positions );
+        transformGroup.addChild(path);
+        path.setSchedulingBounds(new BoundingSphere (new Point3d (0.0, 0.0, 0.0 ), 1000.0));
+        path.setEnable(true);
+        
+        return transformGroup;
     }
     
     public BranchGroup getBranchGroup(){
