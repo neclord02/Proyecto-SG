aedbd071ac563c028eef5ee922a2b41469764865
neclord02 <amglez@correo.ugr.es>
2015-04-08 16:38:15 +0200

    Anillos y texturas ok

--- src/Model/Anillo.java
+++ src/Model/Anillo.java
@@ -1,8 +1,10 @@
 
 package Model;
 
+import com.sun.j3d.utils.image.TextureLoader;
 import java.util.*;
 import javax.media.j3d.*;
+import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
 
@@ -10,6 +12,7 @@ public class Anillo extends Shape3D{
     
     private final BranchGroup bg;
     private Anillo anillo2;
+    private Appearance textura;
     
     public Anillo( float rad1, float rad2, int res1, int res2, Appearance app ){
         
@@ -17,6 +20,7 @@ public class Anillo extends Shape3D{
         
         ArrayList<Point3f> vertex = new ArrayList<>();
         ArrayList<Integer> index = new ArrayList<>();
+        ArrayList<Float> texcoord = new ArrayList<>();
         
         Point3f t = new Point3f( rad2,0,0 );
         Vector3f r = new Vector3f( rad1,0,0 );
@@ -70,6 +74,22 @@ public class Anillo extends Shape3D{
             
         }
         
+        // Generar coordenadas de textura:
+        
+        float startx = 0.0f;
+        float starty = 0.0f;
+        for( int i = 0; i<res1;i++){
+            
+            for( int j = 0; j< res2;j++){
+
+                texcoord.add(startx);
+                texcoord.add(starty);
+                starty += 1.0/res2;
+            }
+            startx += 1.0/res1;
+            starty = 0.0f;
+        }
+        
         Point3f [] coordenadas = new Point3f[ vertex.size() ];
         int [] indices = new int[ index.size() ];
         // Esto es necesario para pasar un arraylist<int> a int []
@@ -78,16 +98,50 @@ public class Anillo extends Shape3D{
             indices[i] = index.get(i);
         
         }
+        
+        float [] textura = new float[ texcoord.size() ];
+        // Esto es necesario para pasar un arraylist<Float> a float []
+        for( int i = 0; i<texcoord.size(); i++){
+        
+            textura[i] = texcoord.get(i);
+        
+        }
+        
         // Tal y como en el pdf
-        IndexedGeometryArray quads = new IndexedQuadArray( vertex.size(),GeometryArray.COORDINATES | GeometryArray.NORMALS ,index.size());
+        IndexedGeometryArray quads = new IndexedQuadArray( vertex.size(), GeometryArray.TEXTURE_COORDINATE_2 | 
+                GeometryArray.COORDINATES | GeometryArray.NORMALS ,index.size());
+        quads.setTextureCoordinates(0, 0, textura);
+        // Los indices para las coordenadas de textura son los mismos que para los puntos del anillo pk estan bien ordenados.
+        quads.setTextureCoordinateIndices(0, 0, indices);
         quads.setCoordinates(0, vertex.toArray(coordenadas));
         quads.setCoordinateIndices(0,indices );
         
         this.setGeometry( quads );
-        this.setAppearance(app);
+        //this.setAppearance(app);
+        this.setTexture();
         
         this.bg.addChild(this);
         
+    }
+    
+    private void setTexture(){
+        this.textura = new Appearance();
+        Texture aTexture= new TextureLoader ("imgs/anillo.png", null).getTexture();
+        
+        this.textura.setMaterial( new Material (
+            new Color3f (0.2f, 0.2f, 0.2f),   // Color ambiental
+            new Color3f (0.2f, 0.2f, 0.2f),   // Color emisivo
+            new Color3f (1.0f, 1.00f, 1.00f),   // Color difuso
+            new Color3f (0.0f, 0.0f, 0.00f),   // Color especular
+            5.0f ) 
+        );
+        
+        TextureAttributes att_t = new TextureAttributes(); 
+        att_t.setTextureMode( TextureAttributes.REPLACE );
+        
+        this.textura.setTextureAttributes(att_t); 
+        this.textura.setTexture(aTexture);
+        this.setAppearance(textura);
     }
     
     public BranchGroup getBranchGroup(){
